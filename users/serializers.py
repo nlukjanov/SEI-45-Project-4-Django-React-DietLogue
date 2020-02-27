fcbaef2d5164c249efa5027b39830adf744519fa
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 19:48:12 +0000

    add populated user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from logs.models import Log, Food
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -6,22 +7,27 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email',
+                  'height', 'weight', 'gender', 'dob', 'profile_image', 'logs')
+
+
+class FoodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Food
         fields = '__all__'
 
 
-# class FoodSerializer(serializers.ModelSerializer):
+class LogSerializer(serializers.ModelSerializer):
 
-#     class Meta:
-#         model = Food
-#         fields = '__all__'
-
-
-# class LogSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Log
-#         fields = '__all__'
+    class Meta:
+        model = Log
+        fields = '__all__'
 
 
-# class PopulatedLogSerializer(LogSerializer):
-#     food = FoodSerializer()
+class PopulatedLogSerializer(LogSerializer):
+    food = FoodSerializer()
+
+
+class PopulatedUserSerializer(UserSerializer):
+    logs = PopulatedLogSerializer(many=True)
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_201_CREATED, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_204_NO_CONTENT, HTTP_202_ACCEPTED, HTTP_401_UNAUTHORIZED, HTTP_200_OK
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PopulatedUserSerializer
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -14,7 +14,7 @@ class UserDetailView(APIView):
     def get(self, request):
         try:
             user = User.objects.get(pk=request.user.id)
-            serialized_user = UserSerializer(user)
+            serialized_user = PopulatedUserSerializer(user)
             return Response(serialized_user.data, status=HTTP_200_OK)
         except User.DoesNotExist:
             return Response({"message: User not Found"}, status=HTTP_404_NOT_FOUND)
