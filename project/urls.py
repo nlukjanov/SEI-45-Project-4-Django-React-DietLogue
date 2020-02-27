e2908026c9c46648c9dad862a6f6f055efdeb1ff
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 18:21:03 +0000

    add views and urls

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('logs/', include('logs.urls'))
+    path('logs/', include('logs.urls')),
+    path('', include('jwt_auth.urls'))
 ]
