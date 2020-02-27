4bf5b22dea44fb9c8b0ed13ad6647015759b6ebb
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 18:14:33 +0000

    change settings to use custom auth

--- project/settings.py
+++ project/settings.py
@@ -125,4 +125,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'jwt_auth.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
 AUTH_USER_MODEL = 'users.User'
