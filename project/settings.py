477dbf611d0d8b80c0e4fbbb6027b79d93904cb8
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 11:39:53 +0000

    created db and switched to postgres

--- project/settings.py
+++ project/settings.py
@@ -73,10 +73,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'diet-log',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
