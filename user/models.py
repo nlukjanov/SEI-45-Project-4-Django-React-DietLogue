a7c9db5768d423881197a2bb8b32ae14bdf73c2a
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 12:49:09 +0000

    added custom user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    height = models.PositiveSmallIntegerField()
+    weight = models.FloatField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    dob = models.DateField(max_length=8)
+    profile_image = models.CharField(max_length=500)
