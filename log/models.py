bbd67e676424794607be82a082a1eeaa9c124e7f
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 16:00:05 +0000

    put food model into log

--- log/models.py
+++ log/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.FloatField()
+    protein = models.FloatField()
+    carbs = models.FloatField()
+    fiber = models.FloatField()
+    fat = models.FloatField()
+    sat_fat = models.FloatField()
+
+
 class Log(models.Model):
-    # food = models.ForeignKey(Food, related_name='food', null=True, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, related_name='food', null=True, on_delete=models.CASCADE)
     portion = models.PositiveSmallIntegerField()
     owner = models.ForeignKey(User, related_name='logs', null=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
