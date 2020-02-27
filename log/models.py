293633e5bfffb139d83e0b6cc83bb5a9da84c0d6
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-27 14:24:37 +0000

    added log model

--- log/models.py
+++ log/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+class Log(models.Model):
+    food = models.ForeignKey(Food, related_name='food', null=True, on_delete=models.CASCADE)
+    portion = models.PositiveSmallIntegerField()
+    owner = models.ForeignKey(User, related_name='logs', null=True, on_delete=models.CASCADE)
