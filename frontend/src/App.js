cbca9347de19e80f094eea2a9d18ff38316a5f40
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-28 12:49:49 +0000

    linter setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  return <div className='App'>hello</div>
 }
 
-export default App;
+export default App
