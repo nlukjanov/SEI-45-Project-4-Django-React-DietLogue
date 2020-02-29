f784f30b8c6ae5210ec947494cff3d1f6880ef93
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-29 19:24:07 +0000

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
+import 'bulma'
 import Home from './components/Home'
 import Register from './components/Register'
 import Login from './components/Login'
 import MyAccount from './components/MyAccount'
 import LogHistory from './components/LogHistory'
 import NewLog from './components/NewLog'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <BrowserRouter>
       <main>
         {/* <Notifications /> */}
-        {/* <Navbar /> */}
+        <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/register' component={Register} />
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+class Navbar extends React.Component {
+
+  // handleLogout = () => {
+  //   notify.show('Come back soon!', 'success', 3000)
+  //   Auth.logout()
+  //   this.props.history.push('/')
+  // }
+  render() {
+    return (
+      <nav className='navbar is-dark'>
+        <div className='container'>
+          <div className='navbar-brand'>
+            <Link className='navbar-item' to='/'>
+              Home
+            </Link>
+          </div>
+          <div
+            className='navbar-menu'
+          >
+            <div className='navbar-end'>
+              <Link className='navbar-item' to='/register'>
+                Register
+              </Link>
+              <Link className='navbar-item' to='/login'>
+                Login
+              </Link>
+              <Link className='navbar-item' to='/myaccount'>
+                My Account
+              </Link>
+              <Link className='navbar-item' to='/loghistory'>
+                Log History
+              </Link>
+              <a onClick={this.handleLogout} className='navbar-item'>
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default withRouter(Navbar)
