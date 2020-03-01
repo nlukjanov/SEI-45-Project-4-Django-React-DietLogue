5d0a7259d3fbdf48d0c1b7fb43686dadd7b4b8c3
cara <xw33@outlook.com>
2020-03-01 13:19:36 +0000

    add basic components in myaccount and loghistory to avoid component error when rendering

---
+++ frontend/src/components/LogHistory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class LogHistory extends React.Component {
+
+    render () {
+      return(
+        <h1>My log history</h1>
+      )
+    }
+}
+
+export default LogHistory  
