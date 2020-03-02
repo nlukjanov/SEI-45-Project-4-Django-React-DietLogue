c0aadd330f3aa6ca7d41e6bf85490d606699e5ae
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-02 09:29:12 +0000

    add navbar burger

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,27 @@ import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
 class Navbar extends React.Component {
+  state = { navbarOpen: false }
+
+  toggleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen })
+  }
+
+  componentDidUpdate(prevProps) {
+    // we have imported withRouter for navbar to know about the current path. if the path has changed, then the navbar will close in mobile vue
+    //! somehow it works even without this code, probably some update in react or bulma
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.setState({ navbarOpen: false })
+    }
+  }
+
   // handleLogout = () => {
   //   notify.show('Come back soon!', 'success', 3000)
   //   Auth.logout()
   //   this.props.history.push('/')
   // }
   render() {
+    const { navbarOpen } = this.state
     return (
       <nav className='navbar is-dark'>
         <div className='container'>
@@ -15,8 +30,19 @@ class Navbar extends React.Component {
             <Link className='navbar-item' to='/'>
               Home
             </Link>
+            <p
+              className={`navbar-burger ${navbarOpen ? 'is-active' : ''}`}
+              onClick={this.toggleNavbar}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </p>
           </div>
-          <div className='navbar-menu'>
+          <div
+            className={`navbar-menu ${navbarOpen ? 'is-active' : ''}`}
+            onClick={this.toggleNavbar}
+          >
             <div className='navbar-end'>
               <Link className='navbar-item' to='/logs/new'>
                 New Log Entry
