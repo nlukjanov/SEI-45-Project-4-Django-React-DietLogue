bb26c283901040fe5935e230ff6af5815aca6a89
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-03 11:53:28 +0000

    fix sorting by date

--- frontend/src/components/LogHistory.js
+++ frontend/src/components/LogHistory.js
@@ -10,15 +10,14 @@ class LogHistory extends Component {
 
   async componentDidMount() {
     try {
-      const res = await axios.get(
-        '/api/myaccount',
-        {
-          headers: {
-            Authorization: `Bearer ${Authentication.getToken('token')}`
-          }
-        } 
+      const res = await axios.get('/api/myaccount', {
+        headers: {
+          Authorization: `Bearer ${Authentication.getToken('token')}`
+        }
+      })
+      const sortedLogs = res.data.logs.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
       )
-      const sortedLogs = res.data.logs.sort((a, b) => b.id - a.id)
       this.setState({ logData: sortedLogs })
     } catch (error) {
       this.props.history.push('/notfound')
@@ -31,9 +30,7 @@ class LogHistory extends Component {
     return (
       <section className='section'>
         <div className='container'>
-          <h1 className='title has-text-centered'>
-            Your log history
-          </h1>
+          <h1 className='title has-text-centered'>Your log history</h1>
           <div className='table-container'>
             <table className='table is-fullwidth'>
               <thead>
@@ -54,21 +51,20 @@ class LogHistory extends Component {
               </thead>
               <tbody>
                 {logData.map(entry => {
-                  console.log(entry.food.sat_fat)
                   return (
                     <tr key={entry.id}>
-                      <td>{moment(entry.created_at).format('DD/MM/YYYY, H:mm')}</td>
+                      <td>{moment(entry.date).format('DD/MM/YYYY')}</td>
                       <td>{entry.food.name}</td>
                       <td>{entry.portion}</td>
                       <td>{entry.food.measure}</td>
                       <td>{entry.food.unit}</td>
-                      <td>{entry.food.grams}</td>
-                      <td>{entry.food.calories}</td>
-                      <td>{entry.food.protein}</td>
-                      <td>{entry.food.carbs}</td>
-                      <td>{entry.food.fiber}</td>
-                      <td>{entry.food.fat}</td>
-                      <td>{entry.food.sat_fat}</td>
+                      <td>{Math.round(entry.food.grams * entry.portion)}</td>
+                      <td>{Math.round(entry.food.calories * entry.portion)}</td>
+                      <td>{Math.round(entry.food.protein * entry.portion)}</td>
+                      <td>{Math.round(entry.food.carbs * entry.portion)}</td>
+                      <td>{Math.round(entry.food.fiber * entry.portion)}</td>
+                      <td>{Math.round(entry.food.fat * entry.portion)}</td>
+                      <td>{Math.round(entry.food.sat_fat * entry.portion)}</td>
                     </tr>
                   )
                 })}
